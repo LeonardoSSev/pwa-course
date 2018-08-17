47976700027b3dede1f8e85b787fb84c2334c168
Leonardo Santos Severino <leonardossev@gmail.com>
2018-08-17 00:35:36 -0300

    Sets exercise

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,90 +1,29 @@
-var deferredPrompt;
 
-if (!window.Promise) {
-    window.Promise = Promise;
-}
+var button = document.querySelector('#start-button');
+var output = document.querySelector('#output');
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(function() {
-        console.log('Service worker registered!');
-    }).catch(function(err) {
-        console.log(err)
-    });
-}
+button.addEventListener('click', function() {
+  // Create a new Promise here and use setTimeout inside the function you pass to the constructor
 
-window.addEventListener('beforeinstallprompt', function(event) {
-    console.log('beforeinstallprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
+  setTimeout(function() { // <- Store this INSIDE the Promise you created!
+    // Resolve the following URL: https://swapi.co/api/people/1
+  }, 3000);
+
+  // Handle the Promise "response" (=> the value you resolved) and return a fetch()
+  // call to the value (= URL) you resolved (use a GET request)
+
+  // Handle the response of the fetch() call and extract the JSON data, return that
+  // and handle it in yet another then() block
+
+  // Finally, output the "name" property of the data you got back (e.g. data.name) inside
+  // the "output" element (see variables at top of the file)
+
+  // Repeat the exercise with a PUT request you send to https://httpbin.org/put
+  // Make sure to set the appropriate headers (as shown in the lecture)
+  // Send any data of your choice, make sure to access it correctly when outputting it
+  // Example: If you send {person: {name: 'Max', age: 28}}, you access data.json.person.name
+  // to output the name (assuming your parsed JSON is stored in "data")
+
+  // To finish the assignment, add an error to URL and add handle the error both as
+  // a second argument to then() as well as via the alternative taught in the module
 });
-
-
-var promise = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-        // resolve('This is executed once the timer is done!')
-        reject({code: 500, message: 'An error occurred!'});
-        // console.log('This is executed once the timer is done');
-    }, 3000);
-});
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://httpbin.org/ip');
-xhr.responseType = 'json';
-
-xhr.onload = function() {
-    console.log(xhr.response);
-};
-
-xhr.onerror = function() {
-    console.log('Error');
-};
-
-xhr.send();
-
-fetch('https://httpbin.org/ip').then(function(response) {
-    console.log(response)
-    return response.json();
-}).then(function(data) {
-    console.log(data);
-}).catch(function(err) {
-    console.log(err);
-});
-
-fetch('https://httpbin.org/post', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    mode: 'cors',
-    body: JSON.stringify({
-        message: 'Does this work?'
-    })
-}).then(function(response) {
-    console.log(response)
-    return response.json();
-}).then(function(data) {
-    console.log(data);
-}).catch(function(err) {
-    console.log(err);
-});
-
-
-// promise.then(function(text) {
-//     return text;
-// }, function(err) {
-//     console.log(err.code, err.message);
-// }).then(function(newText) {
-//     console.log(newText)
-// });
-
-promise.then(function(text) {
-    return text;
-}).then(function(newText) {
-    console.log(newText)
-}).catch(function(err) {
-    console.log(err.code, err.message);
-});
-
-console.log('This is executed right after setTimeout()');
