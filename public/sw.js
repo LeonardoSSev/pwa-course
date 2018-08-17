7befd48a8a8a418af4d60c0d2b43320fac62d8c0
Leonardo Santos Severino <leonardossev@gmail.com>
2018-08-17 01:59:30 -0300

    Versionates cache

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-      caches.open('static')
+      caches.open('static-v3')
           .then(function(cache) {
               console.log('[Service Worker] Precaching App Shell');
               cache.addAll([
@@ -43,6 +43,9 @@ self.addEventListener('fetch', function(event) {
                                   cache.put(event.request.url, res.clone())
                                   return res;
                               });
+                      })
+                      .catch(function(err) {
+
                       });
               }
           })
