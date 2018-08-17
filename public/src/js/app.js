735d0c0f718302763ee7c21cc982034af8443ac5
Leonardo Santos Severino <leonardossev@gmail.com>
2018-08-17 00:07:53 -0300

    Cacthes if Service Worker registering fail

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -3,6 +3,8 @@ var deferredPrompt;
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js').then(function() {
         console.log('Service worker registered!');
+    }).catch(function(err) {
+        console.log(err)
     });
 }
 
