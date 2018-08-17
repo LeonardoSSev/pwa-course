e3fbeed280e5e063ef3f21eac9ddf0521ced1368
Leonardo Santos Severino <leonardossev@gmail.com>
2018-08-17 00:15:20 -0300

    Fetches httpbin with a POST request

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -33,6 +33,23 @@ fetch('https://httpbin.org/ip').then(function(response) {
     console.log(err);
 });
 
+fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        message: 'Does this work?'
+    })
+}).then(function(response) {
+    console.log(response)
+    return response.json();
+}).then(function(data) {
+    console.log(data);
+}).catch(function(err) {
+    console.log(err);
+});
 
 
 // promise.then(function(text) {
